04b237961a8d144da8c35212079dfcee93a5f0cd
Eden Gill <egil0010@student.monash.edu>
2026-03-20 16:41:40 +1100

    applied axios to Login.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -23,12 +23,5 @@ router.get(
   }
 );
 
-router.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views', 'register.html'));
-});
-
-router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views', 'login.html'));
-});
 
 module.exports = router;
